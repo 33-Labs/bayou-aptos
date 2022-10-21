8d88c77e1717e7a86543829ebe3d6baf2844c0c4
lanford33 <lanfordcai@outlook.com>
2022-10-21 22:13:14 +0800

    feat: update home page

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -4,19 +4,30 @@ import Image from "next/image"
 export default function NavigationBar() {
   return (
     <>
-      <div className="relative gap-x-2 flex items-center h-44">
+      <div className="gap-x-2 flex items-center justify-between h-44">
+        <div className="relative gap-x-2 flex items-center">
         <Image src="/bayou.png" alt="" width={50} height={50} priority />
         <label className="font-flow font-bold text-3xl">
           bayou
         </label>
-        {/* <label className="font-flow text-aptos-green border border-aptos-green text-sm whitespace-pre"> {publicConfig.chainEnv} </label> */}
-        <a
-          href={"https://github.com/33-Labs/bayou"}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="absolute right-0">
-          <Image src="/github.png" alt="" width={24} height={24} priority />
-        </a>
+        </div>
+
+        {/* <label className="font-flow text-aptos-green border border-aptos-green text-sm whitespace-pre"> </label> */}
+        <div className="flex gap-x-2 items-center">
+          <div className="relative w-[22px] h-[22px] rounded-full">
+            <Image src="/aptos.svg" alt="" objectPosition="50% 50%" objectFit="contain" layout="fill" priority />
+          </div>
+          <div className="relative w-6 h-6">
+          <a
+            href={"https://flow.bayou33.app"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="">
+            <Image src="/flow.png" alt="" objectPosition="50% 50%" objectFit="contain" layout="fill" priority />
+          </a>
+          </div>
+        </div>
+
       </div>
     </>
   )
